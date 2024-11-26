31eeff0eefc8b40630052fe0bee7d5da76341cea
yaoyangyaha <yaoyangyaha@163.com>
2024-11-26 17:40:15 +0800

    change language to zh-CN

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,7 +11,9 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.use(router)
 
 app.mount('#app')
